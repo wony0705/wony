194d8c7ec8c8b663f12227a7ca93d9f97ead496f
CSW <csw2828@comtec.co.kr>
2021-08-03 10:34:54 +0900

    main,vpc add

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "remote" {
+    organization = "csw-devops"
+
+    workspaces {
+      name = "wony"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-2"
+}
